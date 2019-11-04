ff031d7bf17cd036d83f9892df640bccf053e832
Alberto Antonino Cuffaro <alberto.cuffaro@hotmail.it>
2019-11-04 15:28:46 +0100

    l'app scarica il file, ancora non fa nessun tipo di parsing

--- app/src/main/java/com/reader/groupproject/add_page.kt
+++ app/src/main/java/com/reader/groupproject/add_page.kt
@@ -3,6 +3,7 @@ package com.reader.groupproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -11,20 +12,27 @@ import android.widget.TextView
 
 
 class add_page : AppCompatActivity() {
+    var link: String = ""
+    val search = search_class()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_page)
+
+        Thread(Runnable {
+            search_fun()
+        }).start()
+
         val searchView = findViewById<SearchView>(R.id.search)
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
             override fun onQueryTextSubmit(s: String): Boolean {
-                search_fun(s)
+                set_link(s)
                 return true
             }
 
             override fun onQueryTextChange(s: String): Boolean {
-                search_fun(s)
+                set_link(s)
                 return true
             }
         })
@@ -68,7 +76,23 @@ class add_page : AppCompatActivity() {
         }
     }
 
-    fun search_fun(query: String){
-        findViewById<TextView>(R.id.textView2).text = query
+    fun set_link(new_link: String){
+        this.link = new_link
+    }
+
+    fun search_fun() {
+        while (true) {
+            search.search_fun(link)
+
+            while (search.complete == false) {
+                Thread.sleep(3000)
+            }
+
+            runOnUiThread {
+                findViewById<TextView>(R.id.textView2).text = search.text
+            }
+
+            search.complete = false
+        }
     }
 }
