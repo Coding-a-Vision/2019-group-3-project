b5ad84cfce3a4fbeda87a6cb23a8950d58568b17
Nicola De Pasquale <ndp7c7c0@gmail.com>
2019-11-05 16:33:38 +0100

    aggiunti alcuni titoli

--- app/src/main/java/com/reader/groupproject/MainActivity.kt
+++ app/src/main/java/com/reader/groupproject/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
